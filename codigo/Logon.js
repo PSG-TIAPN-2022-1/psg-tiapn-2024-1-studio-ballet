a89948ba5feaf9df425e1ad7aa6e21c5f5e3a60b
DYOGO <127787843+DYOGO111@users.noreply.github.com>
2024-06-16 16:09:53 -0300

    Create Logon.js

---
+++ codigo/Logon.js
@@ -0,0 +1,98 @@
+let Nome = document.querySelector('#Nome')
+let LabelNome = document.querySelector('#LabelNome')
+let validNome = false;
+
+let Login = document.querySelector('#Login')
+let LabelLogin = document.querySelector('#LabelLogin')
+let validLogin = false;
+
+let Cpf = document.querySelector('#Cpf')
+let LabelCpf = document.querySelector('#LabelCpf')
+let validCpf = false;
+
+let Senha = document.querySelector('#Senha')
+let LabelSenha = document.querySelector('#LabelSenha')
+let validSenha = false;
+
+let ConfirmeSenha = document.querySelector('#ConfirmeSenha')
+let LabelConfirmeSenha = document.querySelector('#LabelConfirmeSenha')
+let validConfirmeSenha = false;
+
+Nome.addEventListener('keyup', () => {
+    if (Nome.value.length <= 2) {
+        Nome.setAttribute('style', 'border-color: red')
+        validNome = false;
+    } else {
+        Nome.setAttribute('style', 'border-color: white')
+        validNome = true;
+    }
+})
+
+Login.addEventListener('keyup', () => {
+    if (Login.value.length <= 4) {
+        Login.setAttribute('style', 'border-color: red')
+        validLogin = false;
+    } else {
+        Login.setAttribute('style', 'border-color: white')
+        validLogin = true;
+    }
+})
+
+Cpf.addEventListener('keyup', () => {
+    if (Cpf.value.length != 11) {
+        Cpf.setAttribute('style', 'border-color: red')
+        validCpf = false;
+    } else {
+        Cpf.setAttribute('style', 'border-color: white')
+        validCpf = true;
+    }
+})
+
+Senha.addEventListener('keyup', () => {
+    if (Senha.value.length <= 7) {
+        Senha.setAttribute('style', 'border-color: red')
+        validSenha = false;
+    } else {
+        Senha.setAttribute('style', 'border-color: white')
+        validSenha = true;
+    }
+})
+
+ConfirmeSenha.addEventListener('keyup', () => {
+    if (Senha.value != ConfirmeSenha.value) {
+        ConfirmeSenha.setAttribute('style', 'border-color: red')
+        validConfirmeSenha = false;
+    } else {
+        ConfirmeSenha.setAttribute('style', 'border-color: white')
+        validConfirmeSenha = true;
+    }
+})
+
+
+
+function cadastrar() {
+
+    if (validNome && validLogin && validCpf && validSenha && validConfirmeSenha) {
+
+        let ListaUser = JSON.parse(localStorage.getItem('ListaUser') || '[]')
+
+        ListaUser.push(
+            {
+                LoginCad: Login.value,
+                CpfCad: Cpf.value,
+                ConfirmeSenhaCad: ConfirmeSenha.value,
+                NomeCad: Nome.value,
+                SenhaCad: Senha.value,
+            }
+        )
+
+        localStorage.setItem('ListaUser',JSON.stringify(ListaUser))
+
+        window.location.href = 'Login.html'
+
+        alert('Cadastrado')        
+        
+    } else {
+        alert('Campo Invalido')
+    }
+}
