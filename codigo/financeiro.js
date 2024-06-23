825bcab7671624d09c39804d9bffd1b184e70dc6
ArturAmendoeira <128499173+ArturAmendoeira@users.noreply.github.com>
2024-06-22 22:44:33 -0300

    Update financeiro.js

--- codigo/financeiro.js
+++ codigo/financeiro.js
@@ -1,11 +1,16 @@
-document.getElementById('consultarPendencias').addEventListener('click', function() {
-    const students = JSON.parse(localStorage.getItem('students')) || [];
-    
-    const hasPendingDues = students.some(student => student.status === 'Pendente');
-    
-    const resultadoConsulta = hasPendingDues 
-        ? 'Você tem pendências com sua mensalidade. Favor entrar em contato com nosso suporte!!' 
+document.getElementById('consultarPendencias').addEventListener('click', async function () {
+    response = await fetch("https://375f818b-2156-47fc-8e41-92f636bfb6ac.mock.pstmn.io/api/Financeiro/01765142016")
+
+    json = await response.json()
+    console.log(json.dados)
+    console.log(json.dados.situacaoPagamento)
+
+    const hasPendingDues = json.dados.situacaoPagamento === 'Pendente';
+
+    const resultadoConsulta = hasPendingDues
+        ? 'Você tem pendências com sua mensalidade. Favor entrar em contato com nosso suporte!!'
         : 'Não há pendências na mensalidade';
-    
+
     document.getElementById('resultadoConsulta').textContent = resultadoConsulta;
+
 });
